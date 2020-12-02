973ad4dcaf0620cf41779a6d63dd5672a2261197
jnjnslab <jnjnslab@gmail.com>
2020-12-02 20:52:54 +0900

    2020.12.02

---
+++ express/basic4/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+app.engine('ejs',ejs.renderFile);
+
+var msg = '';
+
+app.get('/',function(req,res){
+    res.render('temp.ejs',{});
+});
+
+app.post('/ajax', function(req,res){
+    msg += 'hoge';
+    res.json({
+        msg: msg
+    });
+});
+
+var server = app.listen(1234, function(){
+    console.log('サーバーを起動しました');
+});
---
+++ express/basic5/routes/hello.js
@@ -0,0 +1,9 @@
+//生成したひな形に追加
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('hello', { msg: 'Hello World' });
+});
+
+module.exports = router;
